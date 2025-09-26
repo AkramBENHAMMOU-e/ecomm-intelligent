06ef302dcb3ffadf1cc53206dd17cd7c8fdf5239
Akram BENHAMMOU <aben200244@gmail.com>
2025-09-26 17:20:40 +0200

    initialisation d'un proj spring

---
+++ src/main/java/com/proj/ecommintelligent/EcommIntelligentApplication.java
@@ -0,0 +1,13 @@
+package com.proj.ecommintelligent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommIntelligentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommIntelligentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proj/ecommintelligent/EcommIntelligentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.ecommintelligent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommIntelligentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
